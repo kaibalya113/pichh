7ff543877608f02da8b470c10ffa8cb08ab300e0
kaibalya123 <kaibalyamohanty0@gmail.com>
2020-09-20 21:47:01 +0530

    implemented profile page of the user, now user can see his/her posts in profile page

--- app/src/main/java/com/kaibalya/picshh/PostActivity.kt
+++ app/src/main/java/com/kaibalya/picshh/PostActivity.kt
@@ -7,14 +7,18 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.kaibalya.picshh.model.Post
+import com.kaibalya.picshh.model.User
 import kotlinx.android.synthetic.main.activity_post.*
 
 private const val TAG = "PostActivity"
-class PostActivity : AppCompatActivity() {
+private const val EXTRA_USERNAME ="EXTRA_USERNAME"
+open class PostActivity : AppCompatActivity() {
 
+    private var signediInUser: User? =null
     private lateinit var firestoreDb : FirebaseFirestore
     private lateinit var posts: MutableList<Post>
     private lateinit var adapter: PostAdapter
@@ -34,12 +38,32 @@ class PostActivity : AppCompatActivity() {
 
 
         firestoreDb = FirebaseFirestore.getInstance()
-        val postsname = firestoreDb.collection("posts").orderBy("creation_time_ms", Query.Direction.DESCENDING)
+        firestoreDb.collection("users").document(FirebaseAuth.getInstance()
+            .currentUser?.uid as String).get()
+            .addOnSuccessListener { userSnapshot ->
+                signediInUser = userSnapshot.toObject(User::class.java)
+                Log.i(TAG, signediInUser.toString())
+            }
+            .addOnFailureListener{exception ->
+                Log.i(TAG, exception.message)
+            }
+
+
+        var postsname = firestoreDb.collection("posts")
+                                    .orderBy("creation_time_ms", Query.Direction.DESCENDING)
+
+        val username = intent.getStringExtra(EXTRA_USERNAME)
+        if(username != null){
+            supportActionBar?.title = username
+            postsname = postsname.whereEqualTo("user.name", username)
+        }
+
         postsname.addSnapshotListener { snapshot, firebaseFirestoreException ->
             if(snapshot == null || firebaseFirestoreException !=  null){
                 Log.i(TAG, "Caught Exception while retriving", firebaseFirestoreException)
                 return@addSnapshotListener
             }
+
             val postsDocs = snapshot.toObjects(Post::class.java)
             posts.clear()
             posts.addAll(postsDocs)
@@ -49,7 +73,12 @@ class PostActivity : AppCompatActivity() {
             }
         }
         // retrive data from firestore
+        floatingbtn.setOnClickListener {
+            var intent = Intent(this, CreateActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_posts, menu)
@@ -60,6 +89,7 @@ class PostActivity : AppCompatActivity() {
         if(item.itemId == R.id.menu_profile){
             // go to profile activity
             val intent = Intent(this, ProfileActivity::class.java)
+            intent.putExtra(EXTRA_USERNAME, signediInUser?.name)
             startActivity(intent)
         }
         return super.onOptionsItemSelected(item)
